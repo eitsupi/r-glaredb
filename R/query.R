2a822953d5b5865479396787bee6021ac18ce491
eitsupi <ts1s1andn@gmail.com>
2024-05-17 12:38:17 +0000

    feat: resolve table for ExecutionOutput

--- R/query.R
+++ R/query.R
@@ -1,11 +1,10 @@
 #' Run a SQL query against the default in-memory GlareDB database
 #' @param query A character of the SQL query to run
-#' @return [nanoarrow_array_stream]
+#' @return GlareDB execusion output
 #' @export
 #' @examples
 #' glaredb_sql("SELECT 'hello from R' as hello") |>
 #'   as.data.frame()
 glaredb_sql <- function(query) {
-  sql(query) |>
-    as_nanoarrow_array_stream()
+  sql(query)
 }
--- src/rust/src/environment.rs
+++ src/rust/src/environment.rs
@@ -2,6 +2,8 @@ use datafusion::datasource::TableProvider;
 use sqlexec::environment::EnvironmentReader;
 use std::sync::Arc;
 
+use crate::execution::RGlareDbExecutionOutput;
+
 #[derive(Debug, Clone, Copy)]
 pub struct REnvironmentReader;
 
@@ -11,7 +13,24 @@ impl EnvironmentReader for REnvironmentReader {
         &self,
         name: &str,
     ) -> Result<Option<Arc<dyn TableProvider>>, Box<dyn std::error::Error + Send + Sync>> {
-        // TODO: implement this
+        let classes = savvy::StringSexp(
+            savvy::eval_parse_text(format!(r#"base::get0("{name}") |> class()"#))
+                .unwrap()
+                .inner(),
+        )
+        .to_vec();
+
+        if classes.iter().any(|&s| s == "RGlareDbExecutionOutput") {
+            let sexp = savvy::Sexp(
+                savvy::eval_parse_text(format!(r#"base::get0("{name}")$.ptr"#))
+                    .unwrap()
+                    .inner(),
+            );
+            let exec = RGlareDbExecutionOutput::try_from(sexp).unwrap();
+
+            return Ok(Some(Arc::new(exec) as Arc<dyn TableProvider>));
+        }
+
         Ok(None)
     }
 }
