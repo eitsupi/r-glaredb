1d89b994ca14a7c8a09eac3802bcc5fa0abb3bca
eitsupi <ts1s1andn@gmail.com>
2024-05-18 13:35:32 +0000

    feat: glaredb_connect function to create connection object

---
+++ R/connect.R
@@ -0,0 +1,31 @@
+#' Connect to a GlareDB database
+#'
+#' @param data_dir_or_cloud_url A character of path to a local GlareDB database or
+#' a cloud URL or `NULL`. If `NULL`, a in-memory database is used.
+#' @param ... Ignored.
+#' @param spill_path TODO
+#' @param disable_tls `TRUE` or `FALSE` to indicating whether to disable TLS.
+#' @param cloud_addr A character of a GlareDB cloud URL.
+#' @param location TODO
+#' @return GlareDB connection object
+#' @export
+#' @examples
+#' con <- glaredb_connect()
+#'
+#' con$sql("SELECT 'hello from R' as hello") |>
+#'   as.data.frame()
+glaredb_connect <- function(
+    data_dir_or_cloud_url = NULL,
+    ...,
+    spill_path = NULL,
+    disable_tls = FALSE,
+    cloud_addr = "https://console.glaredb.com",
+    location = NULL) {
+  connect(
+    cloud_addr = cloud_addr,
+    disable_tls = disable_tls,
+    data_dir_or_cloud_url = data_dir_or_cloud_url,
+    spill_path = spill_path,
+    location = location
+  )
+}
--- src/rust/src/connect.rs
+++ src/rust/src/connect.rs
@@ -5,29 +5,28 @@ use crate::runtime::GLOBAL_RUNTIME;
 use savvy::savvy;
 use std::sync::Arc;
 
-// TODO: support optional arguments with savvy > 0.6.3
 #[savvy]
 pub fn connect(
-    // data_dir_or_cloud_url: &str,
-    // spill_path: &str,
-    // disable_tls: bool,
     cloud_addr: &str,
-    // location: &str,
+    disable_tls: bool,
+    data_dir_or_cloud_url: Option<&str>,
+    spill_path: Option<&str>,
+    location: Option<&str>,
     // storage_options: Option<HashMap<String, String>>, // TODO: support storage options
 ) -> savvy::Result<RGlareDbConnection> {
-    // let data_dir_or_cloud_url = data_dir_or_cloud_url.to_string();
-    // let spill_path = spill_path.to_string();
+    let data_dir_or_cloud_url = data_dir_or_cloud_url.map(|s| s.to_string());
+    let spill_path = spill_path.map(|s| s.to_string());
     let cloud_addr = cloud_addr.to_string();
-    // let location = location.to_string();
+    let location = location.map(|s| s.to_string());
 
     GLOBAL_RUNTIME.0.block_on(async move {
         Ok(RGlareDbConnection {
             inner: Arc::new(
                 glaredb::ConnectOptionsBuilder::default()
-                    .connection_target(None)
-                    .location(None)
-                    .spill_path(None)
-                    .disable_tls(false)
+                    .connection_target(data_dir_or_cloud_url.clone())
+                    .location(location)
+                    .spill_path(spill_path)
+                    .disable_tls(disable_tls)
                     .cloud_addr(cloud_addr)
                     .client_type(sqlexec::remote::client::RemoteClientType::Rust)
                     .environment_reader(Arc::new(REnvironmentReader))
