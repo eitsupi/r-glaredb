52d4817b384cd05cb2cf48f642fdfbc1172c7db6
tycho garen <garen@tychoish.com>
2024-08-30 12:37:13 -0400

    fix: import

--- src/rust/src/environment.rs
+++ src/rust/src/environment.rs
@@ -2,7 +2,6 @@ use std::sync::{Arc, Mutex};
 
 use glaredb::ext::datafusion::datasource::{MemTable, TableProvider};
 use glaredb::ext::EnvironmentReader;
-use glaredb::EnvironmentReader;
 use savvy::ffi::SEXP;
 use savvy::protect::{insert_to_preserved_list, release_from_preserved_list};
 use savvy::{EnvironmentSexp, Sexp};
